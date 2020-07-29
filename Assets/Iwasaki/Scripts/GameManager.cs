27f6f92c400d5f9f621be9ce9613859cc7b8b5b7
letconst <19c1020004et@edu.tech.ac.jp>
2020-07-30 02:48:58 +0900

    [Improve] シーン遷移時、ゲームクリアまたはゲームオーバーかを判別して値をリセットしてあげるように

--- Assets/Iwasaki/Scripts/GameManager.cs
+++ Assets/Iwasaki/Scripts/GameManager.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class GameManager : SingletonMonoBehaviour<GameManager>
-{    
+{
+    public PlayerInventoryContainer playerContainer;
+    public InventoryContainerBase largePlateContainer;
+
+    // 現在のシーン
+    private Scene _currentScene;
+    private Scene _tmpScene;
+
     private float noiseValue;
 
     public float NoiseMator
@@ -25,5 +34,31 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
             return;
         }
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        _currentScene = SceneManager.GetActiveScene();
+
+        // シーン遷移後、現在のシーンがゲームクリアまたはゲームオーバーなら値をリセットしてあげる
+        if (_currentScene.name != _tmpScene.name &&
+            (_currentScene.name == "GameClear" ||
+             _currentScene.name == "GameOverScenes"))
+        {
+            ResetAllValues();
+        }
+
+        _tmpScene = SceneManager.GetActiveScene();
+    }
+
+    /// <summary>
+    /// すべての値をリセットする
+    /// </summary>
+    public void ResetAllValues()
+    {
+        playerContainer.Container.Clear();
+        largePlateContainer.Container.Clear();
+        TimeCounter.currentTime = TimeCounter.countup;
+        NoiseMator = 0;
     }
 }
