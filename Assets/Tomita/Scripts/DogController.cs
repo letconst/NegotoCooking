5f4c07437f588fdf9a0423bfde15dd49087e4b9c
RIZE1223 <airhideplus1223@icloud.com>
2020-08-28 15:44:45 +0900

    犬の検知範囲をやったー！

---
+++ Assets/Tomita/Scripts/DogController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogController : MonoBehaviour
+{
+    public enum DogState
+    {
+        Idle,
+        FindFood,
+        FindPlayer
+    }
+    private DogState State = DogState.Idle;
+    private bool isNearPlayer;
+    [SerializeField]
+    private SphereCollider searchArea;
+    [SerializeField]
+    private float searchAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //主人公の方向
+            var playerDirection = other.transform.position - transform.position;
+            //敵の前方から主人公の方向
+            var angle = Vector3.Angle(transform.forward, playerDirection);
+            //サーチする角度内だったら発見
+            if(angle<=searchAngle)
+            {
+                Debug.Log("主人公発見");
+                State = DogState.FindPlayer;
+            }
+        }
+    }
+}
