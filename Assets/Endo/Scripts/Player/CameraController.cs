b6a8e6f90d169cac366c4448f8a31313c1afdf8e
letconst <19c1020004et@edu.tech.ac.jp>
2020-09-08 22:53:39 +0900

    [Improve] カメラの回転仮対応

--- Assets/Endo/Scripts/Player/CameraController.cs
+++ Assets/Endo/Scripts/Player/CameraController.cs
@@ -1,46 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //プレイヤーを変数に格納
-    private GameObject _player;
-    private float _playerPos;
-    //回転させるスピード
-    public float rotateSpeed = 3.0f;
+    [SerializeField, Tooltip("回転スピード")]
+    private float rotateSpeed = 3.0f;
 
-    public Player MyP { get; private set; }
+    [SerializeField, Tooltip("Zの移動を固定する座標値")]
+    private float minFrontPos;
+
+    // プレイヤーオブジェクト
+    private GameObject _player;
+
+    // カメラの親（軸）オブジェクト
+    private GameObject _wrapper;
 
     // Start is called before the first frame update
     private void Start()
     {
-        // プレイヤーオブジェクト取得
-        //_player = GameObject.FindGameObjectWithTag("Player");
-        MyP = FindObjectOfType<Player>();
+        _player  = GameObject.FindGameObjectWithTag("Player");
+        _wrapper = transform.parent.gameObject;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        var playerPos = MyP.transform.position;
+        var playerPos = _player.transform.position;
 
-        if (playerPos.z >= -22)
+        if (playerPos.z >= minFrontPos)
         {
-            // カメラの位置をプレイヤーに追従
-            transform.position = new Vector3(playerPos.x, playerPos.y + 15, playerPos.z - 15);
+            // 軸をプレイヤーに追従
+            _wrapper.transform.position = playerPos;
         }
         else
         {
-            // カメラの位置をプレイヤーに追従
-            transform.position = new Vector3(playerPos.x, playerPos.y + 15, -37);
+            // 軸をプレイヤーに追従
+            _wrapper.transform.position = new Vector3(playerPos.x, playerPos.y, minFrontPos);
         }
 
         //回転させる角度
         float angle = Input.GetAxis("R_Stick_H") * rotateSpeed;
- 
-        //プレイヤー位置情報
-        Vector3 _playerPos = MyP.transform.position;
 
         //カメラを回転させる
         transform.RotateAround(playerPos, Vector3.up, angle);
