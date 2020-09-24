0755a6249553a73b61f980ae894826e09578da90
岩崎友哉 <tomoti2525@yahoo.co.jp>
2020-09-24 22:22:04 +0900

    マージ前

--- Assets/Iwasaki/Scripts/ResultAnim.cs
+++ Assets/Iwasaki/Scripts/ResultAnim.cs
@@ -7,24 +7,31 @@ public class ResultAnim : MonoBehaviour
     [SerializeField]
     private Animator anim;
     [SerializeField]
+    private ParticleSystem kirakira;
+    [SerializeField]
     private ParticleSystem good;
     [SerializeField]
     private ParticleSystem bad;
-    private int hyouka = 2;
+    private int hyouka = 1;
     [SerializeField]
     private GameObject goodSoup;
     [SerializeField]
     private GameObject badSoup;
+    [SerializeField]
+    private GameObject clearTime;
+    [SerializeField]
+    private GameObject toTitleButton;
     void Start()
     {
         good.Stop();
         bad.Stop();
+        kirakira.Stop();
         StartCoroutine(WaitTime(2.0f));
-        if (hyouka == 1)
+        if (hyouka == 1 || hyouka == 2)
         {
             goodSoup.gameObject.SetActive(true);
         }
-        else if (hyouka == 2)
+        else if (hyouka == 3)
         {
             badSoup.gameObject.SetActive(true);
         }
@@ -39,16 +46,26 @@ public class ResultAnim : MonoBehaviour
     private IEnumerator WaitTime(float waittime)
     {
         yield return new WaitForSeconds(waittime);
-        anim.SetTrigger("ResultAnim");
+        anim.SetTrigger("Cloche");
         yield return new WaitForSeconds(1.0f);
         if (hyouka == 1)
         {
             good.Play();
+            kirakira.Play();
         }
         else if(hyouka == 2)
+        {
+            good.Play();
+        }
+        else if(hyouka == 3)
         {
             bad.Play();
         }
         yield break;
     }   
+    public void FinishAnim()
+    {
+        clearTime.gameObject.SetActive(true);
+        toTitleButton.gameObject.SetActive(true);
+    }
 }
