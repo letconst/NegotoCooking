aaca19719e43c77947a878cf235957b82d8cf754
letconst <2000takeru93@gmail.com>
2020-09-24 21:18:41 +0900

    [Fix] マージによる競合部分の修正

--- Assets/Endo/Scripts/Item/Inventory/PlayerInventoryContainer.cs
+++ Assets/Endo/Scripts/Item/Inventory/PlayerInventoryContainer.cs
@@ -7,26 +7,22 @@ public class PlayerInventoryContainer : InventoryContainerBase
 {
     [SerializeField]
     private int maxDogFoodCount = 4;
+
     [SerializeField]
     private int dogFoodCount;
 
-    public int DogFoodCount 
-    { 
-        get => dogFoodCount;
-        set => dogFoodCount = Mathf.Clamp(value, 0, MaxDogFoodCount);
-    }
-    public int MaxDogFoodCount { get => maxDogFoodCount; }
+    public int DogFoodCount    { get => dogFoodCount; set => dogFoodCount = Mathf.Clamp(value, 0, MaxDogFoodCount); }
+    public int MaxDogFoodCount => maxDogFoodCount;
 
-    public override void AddItem(Item item, List<FoodState> states)
+    public override void AddSlot(Item item, IEnumerable<FoodState> states)
     {
         // インベントリサイズ以上は追加しない（暫定）
         if (Container.Count >= 3) return;
 
-        base.AddItem(item, states);
+        base.AddSlot(item, states);
     }
 
     // TODO: 投げ餌専用の管理
 }
-
 
 //餌用の持たせる領域の確保
