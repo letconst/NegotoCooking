21b7fa97c537f342fd0a26244b4c6e043589f342
letconst <2000takeru93@gmail.com>
2020-10-20 05:22:13 +0900

    [Update] 残り制限時間により、徐々に朝アイコンが表示されるように

--- Assets/Tomita/TimeCounter.cs
+++ Assets/Tomita/TimeCounter.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class TimeCounter : MonoBehaviour
@@ -16,14 +18,40 @@ public class TimeCounter : MonoBehaviour
     [SerializeField]
     private Text timeText;
 
+    [SerializeField]
+    private Image nightIcon;
+
+    [SerializeField]
+    private Image morningIcon;
+
+    private Vector3 _morningPos;
+
+    private Vector3 _nightPos;
+
+    private IEnumerator Start()
+    {
+        // 何故か開始時と1フレーム後とで各アイコンの座標値が変化するので、1フレーム待機
+        yield return null;
+
+        _morningPos = morningIcon.transform.position;
+        _nightPos   = nightIcon.transform.position;
+    }
+
     // Update is called once per frame
     private void Update()
     {
-        //時間をカウントする
+        // 朝アイコンの座標が取得されるまで待機（↑の1フレーム待機による処置）
+        if (_morningPos == default) return;
+
+        // 時間をカウントする
         CurrentTime -= Time.deltaTime;
 
-        //時間を表示する
-        timeText.text = CurrentTime.ToString("0");
+        // 時間を表示する
+        // 時間を表示しなくなったことによりCO
+        // timeText.text = CurrentTime.ToString("0");
+
+        // 朝アイコンを残り時間に応じて徐々に表示
+        morningIcon.transform.position = Vector3.Lerp(_nightPos, _morningPos, CurrentTime / CountUp);
 
         // タイムアップでゲームオーバー
         if (CurrentTime <= timeLimit)
