761952c97f76a5c09a20aa6865016220192ddd49
letconst <2000takeru93@gmail.com>
2020-10-31 21:05:43 +0900

    [Fix] 調理から戻った際、所持品等のUIが表示されなくなるのを修正

--- Assets/Iwasaki/Scripts/Menu.cs
+++ Assets/Iwasaki/Scripts/Menu.cs
@@ -12,29 +12,44 @@ public class Menu : MonoBehaviour
     private GameObject dogToyCount;
     [SerializeField]
     private GameObject playeInv;
+
+    private CanvasGroup _playerInvCanvasGroup;
+    private CanvasGroup _noiseGuageCanvasGroup;
+    private CanvasGroup _dogToyCountCanvasGroup;
+
     // Start is called before the first frame update
     void Start()
     {
+        _playerInvCanvasGroup   = playeInv.GetComponent<CanvasGroup>();
+        _noiseGuageCanvasGroup  = noiseGauge.GetComponent<CanvasGroup>();
+        _dogToyCountCanvasGroup = dogToyCount.GetComponent<CanvasGroup>();
+
         menu.SetActive(false);
+
         if (GameManager.Instance.menuBool)
         {
+            _playerInvCanvasGroup.alpha   = 0;
+            _noiseGuageCanvasGroup.alpha  = 0;
+            _dogToyCountCanvasGroup.alpha = 0;
+
             menu.SetActive(true);
             Time.timeScale = 0;
-        }             
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("joystick button 0") && GameManager.Instance.menuBool || 
+        if (Input.GetKeyDown("joystick button 0") && GameManager.Instance.menuBool ||
             Input.GetKeyDown(KeyCode.Q) && GameManager.Instance.menuBool)
         {
             GameManager.Instance.menuBool = false;
-            Time.timeScale = 1;
             menu.SetActive(false);
-            noiseGauge.GetComponent<CanvasGroup>().alpha = 1;
-            dogToyCount.GetComponent<CanvasGroup>().alpha = 1;
-            playeInv.GetComponent<CanvasGroup>().alpha = 1;
+            Time.timeScale = 1;
+
+            _playerInvCanvasGroup.alpha = 1;
+            _noiseGuageCanvasGroup.alpha = 1;
+            _dogToyCountCanvasGroup.alpha = 1;
         }
     }
 }
