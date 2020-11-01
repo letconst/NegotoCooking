0db4fd3ca73415e9c28dd8cd7918f0d05aa52281
letconst <2000takeru93@gmail.com>
2020-11-01 20:15:12 +0900

    [Fix] 基本操作説明画面において連打すると、メインシーンの読み込みが複数実行されてしまうのを修正

--- Assets/Endo/Scripts/Util/SlideShow.cs
+++ Assets/Endo/Scripts/Util/SlideShow.cs
@@ -11,6 +11,9 @@ public class SlideShow : MonoBehaviour
     // スライドショーの画像を表示する領域
     private Image _placeholder;
 
+    // シーン読み込みか否か
+    private bool _isSceneLoading;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -29,7 +32,8 @@ public class SlideShow : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (!Input.GetButtonDown("Submit")) return;
+        // A押下時およびシーン読み込み中でなければ動作
+        if (!Input.GetButtonDown("Submit") || _isSceneLoading) return;
 
         // 現在表示してる画像のインデックス
         var curImageIndex = showImage.IndexOf(_placeholder.sprite);
@@ -37,6 +41,7 @@ public class SlideShow : MonoBehaviour
         // 最後の画像に到達したらゲームシーンに遷移
         if (curImageIndex == showImage.Count - 1)
         {
+            _isSceneLoading = true;
             SceneChanger.Instance.SceneLoad(SceneChanger.SceneName.GameScenes, true);
 
             return;
