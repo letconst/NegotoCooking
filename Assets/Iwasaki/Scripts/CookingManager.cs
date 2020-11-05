0baeeadb591d90587f3d9efecbe71ef86a7bf5d0
letconst <2000takeru93@gmail.com>
2020-11-05 22:49:27 +0900

    [Fix] 焼きおよび煮込み調理中、焦げや泡の溢れが発生しなくなっていたのを修正。また各調理シーンからプレイしたときにエラーが出なくなるように

--- Assets/Iwasaki/Scripts/CookingManager.cs
+++ Assets/Iwasaki/Scripts/CookingManager.cs
@@ -1,37 +1,45 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CookingManager : SingletonMonoBehaviour<CookingManager>
 {
     private float timeleft;
     private float burntTimeleft;
-    private bool doOnceAlert = true;
-    private bool go100Point;
+    private bool  doOnceAlert = true;
+    private bool  go100Point;
+    private bool  _isNowCooking;
+
     public void Awake()
     {
         if (this != Instance)
         {
             Destroy(gameObject);
+
             return;
         }
-        DontDestroyOnLoad(gameObject);
-    }
-    void Start()
-    {
 
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        var curSceneName = SceneManager.GetActiveScene().name;
+        _isNowCooking = curSceneName == "BakeScenes" || curSceneName == "BoilScenes" || curSceneName == "CutScenes";
+
+        // 調理中でなければ終了
+        if (!_isNowCooking) return;
+
         if (FireControl_boil.bubbleBool)
         {
             timeleft -= Time.deltaTime;
+
             if (timeleft <= 0.0)
             {
                 //Debug.Log("PotAction");
                 timeleft = 1.0f;
+
                 //中だと1秒で10ポイント増える。強だと1秒で25ポイント。100ポイント溜まってしまうと泡があふれて警報が鳴る。
                 if (GameManager.Instance.FireChange == 1)
                 {
@@ -47,9 +55,11 @@ public class CookingManager : SingletonMonoBehaviour<CookingManager>
         if (FireControl.burntBool)
         {
             burntTimeleft -= Time.deltaTime;
+
             if (burntTimeleft <= 0.0)
             {
                 burntTimeleft = 1.0f;
+
                 if (GameManager.Instance.FireChange == 1)
                 {
                     GameManager.Instance.BakePoint += 5;
@@ -64,12 +74,13 @@ public class CookingManager : SingletonMonoBehaviour<CookingManager>
         if (GameManager.Instance.BubblePoint == 100 && doOnceAlert)
         {
             SoundManager.Instance.PlayBgm(BGM.Alert);
-            go100Point = true;
-            doOnceAlert = false;
+            go100Point                     = true;
+            doOnceAlert                    = false;
             GameManager.Instance.alertBool = true;
         }
 
-        if (GameManager.Instance.BubblePoint < 100 && !doOnceAlert)
+        if (GameManager.Instance.BubblePoint < 100 &&
+            !doOnceAlert)
         {
             if (go100Point)
             {
@@ -77,14 +88,18 @@ public class CookingManager : SingletonMonoBehaviour<CookingManager>
                 SoundManager.Instance.FadeOutBgm(0.1f);
                 StartCoroutine(waitTime(0.2f));
             }
-            doOnceAlert = true;
+
+            doOnceAlert                    = true;
             GameManager.Instance.alertBool = false;
         }
     }
+
     private IEnumerator waitTime(float waittime)
     {
         yield return new WaitForSeconds(waittime);
+
         SoundManager.Instance.PlayBgm(BGM.BoilSound, isLoop: true);
+
         yield break;
     }
 }
